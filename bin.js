e5796d31826d258c19595c9b915513af63b11130
Michael Williams <michael.williams@enspiral.com>
2018-04-04 15:00:33 -0700

    support auto-reconnecting sbot

--- bin.js
+++ bin.js
@@ -1,6 +1,49 @@
 #!/usr/bin/env node
 
-require('ssb-client')(function (err, sbot, config) {
-  if (err) throw err
+var Reconnect = require('pull-reconnect')
+var Client = require('ssb-client')
+var Logger = require('pino')
+
+var sbot = createClient(function (sbot, config) {
   require('.').init(sbot, config)
 })
+
+function createClient (cb) {
+  var client
+  var config
+  var setup = false
+  var logger = Logger()
+
+  var rec = Reconnect(function (notify) {
+    Client(function (err, _client, _config) {
+      if (err) {
+        logger.error(err)
+        notify(err)
+        return
+      }
+
+      client = _client
+      config = _config
+
+      config.logger = logger
+
+      if (!setup) {
+        cb(sbot, config)
+        setup = true
+      }
+
+      client.on('closed', () => {
+        client = null
+        notify(new Error('closed'))
+      })
+
+      notify()
+    })
+  })
+
+  var sbot = {
+    whoami: rec.async(function (id, cb) {
+      return client.whoami(id, cb)
+    })
+  }
+}
